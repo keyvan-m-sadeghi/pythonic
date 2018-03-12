c97be85d9d65b35ce877bb0c008ff01d38d3167a
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2018-03-12 12:53:31 +0330

    implemented range and Generator

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-function * keyValues(obj) {
-    for (const key of Object.keys(obj)) {
-        yield [key, obj[key]];
-    }
-}
-
 function * enumerate(iterable) {
     let index = 0;
     for (const element of iterable) {
@@ -24,4 +18,57 @@ function * zip(iterFirst, iterSecond) {
     }
 }
 
-module.exports = {keyValues, enumerate, zip};
+function * keyValues(obj) {
+    for (const key of Object.keys(obj)) {
+        yield [key, obj[key]];
+    }
+}
+
+class Generator {
+    constructor(generatorFn) {
+        this[Symbol.iterator] = generatorFn;
+    }
+
+    map(callbackFn) {
+        const result = [];
+        for (const element of this) {
+            result.push(callbackFn(element));
+        }
+        return result;
+    }
+
+    filter(callbackFn) {
+        const result = [];
+        for (const element of this) {
+            if (callbackFn(element)) {
+                result.push(element);
+            }
+        }
+        return result;
+    }
+
+    toArray() {
+        return Array.from(this);
+    }
+}
+
+const rangeGeneratorWithStop = stop => function * () {
+    for (let i = 0; i < stop; i++) {
+        yield i;
+    }
+};
+
+const rangeGeneratorWithSartAndStopAndStep = (start, stop, step = 1) => function * () {
+    for (let i = start; i < stop; i += step) {
+        yield i;
+    }
+};
+
+function range(...args) {
+    if (args.length < 2) {
+        return new Generator(rangeGeneratorWithStop(...args));
+    }
+    return new Generator(rangeGeneratorWithSartAndStopAndStep(...args));
+}
+
+module.exports = {range, enumerate, zip, keyValues, Generator};
--- test.js
+++ test.js
@@ -1,24 +1,19 @@
 const test = require('ava');
-const {keyValues, enumerate, zip} = require('.');
+const {range, enumerate, zip, keyValues} = require('.');
 
-test('keyValues', t => {
-    const obj = {
-        one: 1,
-        two: 2,
-        three: 3
-    };
+test('range with stop', t => {
+    t.deepEqual(range(3).map(x => x + 1), [1, 2, 3]);
+    t.deepEqual(range(3).filter(x => x > 0), [1, 2]);
+});
 
-    for (const [key, value] of keyValues(obj)) {
-        console.log(key, value);
-        t.is(value, obj[key]);
-    }
+test('range with start, stop and step', t => {
+    t.deepEqual(range(-10, 20, 10).toArray(), [-10, 0, 10]);
 });
 
 test('enumerate', t => {
     const iter = [0, 1, 2];
 
     for (const [index, value] of enumerate(iter)) {
-        console.log(index);
         t.is(index, value);
     }
 });
@@ -43,10 +38,21 @@ test('zip', t => {
     for (const {iterFirst, iterSecond} of scenarios) {
         index = 0;
         for (const [first, second] of zip(iterFirst, iterSecond)) {
-            console.log(first, second);
             t.is(first, iterFirst[index]);
             t.is(second, iterSecond[index]);
             index++;
         }
+    }
+});
+
+test('keyValues', t => {
+    const obj = {
+        one: 1,
+        two: 2,
+        three: 3
+    };
+
+    for (const [key, value] of keyValues(obj)) {
+        t.is(value, obj[key]);
     }
 });
